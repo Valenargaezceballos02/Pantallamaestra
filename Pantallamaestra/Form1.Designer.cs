d8e803d0539e8b66ab97ba8864d62632743e1be9
Valen <Valen@DESKTOP-5C9I1IT>
2023-10-23 11:06:10 -0500

    Agregar archivos de proyecto.

---
+++ Pantallamaestra/Form1.Designer.cs
@@ -0,0 +1,288 @@
+﻿namespace Pantallamaestra
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvRegistro = new System.Windows.Forms.DataGridView();
+            this.Datos = new System.Windows.Forms.GroupBox();
+            this.txttelefono = new System.Windows.Forms.TextBox();
+            this.txtapellidos = new System.Windows.Forms.TextBox();
+            this.txtnombres = new System.Windows.Forms.TextBox();
+            this.txtcodigo = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.accion = new System.Windows.Forms.GroupBox();
+            this.btnbuscar = new System.Windows.Forms.Button();
+            this.btneliminar = new System.Windows.Forms.Button();
+            this.btnmodificar = new System.Windows.Forms.Button();
+            this.btnagregar = new System.Windows.Forms.Button();
+            this.registros = new System.Windows.Forms.GroupBox();
+            this.txtbuscar = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvRegistro)).BeginInit();
+            this.Datos.SuspendLayout();
+            this.accion.SuspendLayout();
+            this.registros.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dgvRegistro
+            // 
+            this.dgvRegistro.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvRegistro.Location = new System.Drawing.Point(17, 81);
+            this.dgvRegistro.Name = "dgvRegistro";
+            this.dgvRegistro.RowHeadersWidth = 62;
+            this.dgvRegistro.RowTemplate.Height = 28;
+            this.dgvRegistro.Size = new System.Drawing.Size(954, 203);
+            this.dgvRegistro.TabIndex = 0;
+            this.dgvRegistro.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvRegistro_CellContentClick);
+            // 
+            // Datos
+            // 
+            this.Datos.Controls.Add(this.txttelefono);
+            this.Datos.Controls.Add(this.txtapellidos);
+            this.Datos.Controls.Add(this.txtnombres);
+            this.Datos.Controls.Add(this.txtcodigo);
+            this.Datos.Controls.Add(this.label4);
+            this.Datos.Controls.Add(this.label3);
+            this.Datos.Controls.Add(this.label2);
+            this.Datos.Controls.Add(this.label1);
+            this.Datos.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Datos.Location = new System.Drawing.Point(34, 12);
+            this.Datos.Name = "Datos";
+            this.Datos.Size = new System.Drawing.Size(639, 185);
+            this.Datos.TabIndex = 1;
+            this.Datos.TabStop = false;
+            this.Datos.Text = "Datos";
+            // 
+            // txttelefono
+            // 
+            this.txttelefono.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txttelefono.Location = new System.Drawing.Point(450, 102);
+            this.txttelefono.Name = "txttelefono";
+            this.txttelefono.Size = new System.Drawing.Size(178, 30);
+            this.txttelefono.TabIndex = 7;
+            // 
+            // txtapellidos
+            // 
+            this.txtapellidos.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtapellidos.Location = new System.Drawing.Point(419, 51);
+            this.txtapellidos.Name = "txtapellidos";
+            this.txtapellidos.Size = new System.Drawing.Size(209, 30);
+            this.txtapellidos.TabIndex = 6;
+            // 
+            // txtnombres
+            // 
+            this.txtnombres.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtnombres.Location = new System.Drawing.Point(109, 108);
+            this.txtnombres.Name = "txtnombres";
+            this.txtnombres.Size = new System.Drawing.Size(222, 30);
+            this.txtnombres.TabIndex = 5;
+            // 
+            // txtcodigo
+            // 
+            this.txtcodigo.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtcodigo.Location = new System.Drawing.Point(93, 51);
+            this.txtcodigo.Name = "txtcodigo";
+            this.txtcodigo.Size = new System.Drawing.Size(100, 30);
+            this.txtcodigo.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(321, 51);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(92, 25);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Apellidos";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(355, 108);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Telefono";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 108);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nombres";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Codigo";
+            // 
+            // accion
+            // 
+            this.accion.Controls.Add(this.btnbuscar);
+            this.accion.Controls.Add(this.btneliminar);
+            this.accion.Controls.Add(this.btnmodificar);
+            this.accion.Controls.Add(this.btnagregar);
+            this.accion.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.accion.Location = new System.Drawing.Point(695, 27);
+            this.accion.Name = "accion";
+            this.accion.Size = new System.Drawing.Size(329, 170);
+            this.accion.TabIndex = 2;
+            this.accion.TabStop = false;
+            this.accion.Text = "Accion a Realizar";
+            // 
+            // btnbuscar
+            // 
+            this.btnbuscar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnbuscar.Location = new System.Drawing.Point(199, 99);
+            this.btnbuscar.Name = "btnbuscar";
+            this.btnbuscar.Size = new System.Drawing.Size(99, 41);
+            this.btnbuscar.TabIndex = 3;
+            this.btnbuscar.Text = "Buscar";
+            this.btnbuscar.UseVisualStyleBackColor = true;
+            this.btnbuscar.Click += new System.EventHandler(this.btnbuscar_Click);
+            // 
+            // btneliminar
+            // 
+            this.btneliminar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btneliminar.Location = new System.Drawing.Point(199, 42);
+            this.btneliminar.Name = "btneliminar";
+            this.btneliminar.Size = new System.Drawing.Size(99, 38);
+            this.btneliminar.TabIndex = 2;
+            this.btneliminar.Text = "Eliminar";
+            this.btneliminar.UseVisualStyleBackColor = true;
+            this.btneliminar.Click += new System.EventHandler(this.btneliminar_Click);
+            // 
+            // btnmodificar
+            // 
+            this.btnmodificar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnmodificar.Location = new System.Drawing.Point(38, 99);
+            this.btnmodificar.Name = "btnmodificar";
+            this.btnmodificar.Size = new System.Drawing.Size(101, 41);
+            this.btnmodificar.TabIndex = 1;
+            this.btnmodificar.Text = "Modificar";
+            this.btnmodificar.UseVisualStyleBackColor = true;
+            this.btnmodificar.Click += new System.EventHandler(this.btnmodificar_Click);
+            // 
+            // btnagregar
+            // 
+            this.btnagregar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnagregar.Location = new System.Drawing.Point(38, 41);
+            this.btnagregar.Name = "btnagregar";
+            this.btnagregar.Size = new System.Drawing.Size(101, 39);
+            this.btnagregar.TabIndex = 0;
+            this.btnagregar.Text = "Agregar";
+            this.btnagregar.UseVisualStyleBackColor = true;
+            this.btnagregar.Click += new System.EventHandler(this.btnagregar_Click);
+            // 
+            // registros
+            // 
+            this.registros.Controls.Add(this.txtbuscar);
+            this.registros.Controls.Add(this.label5);
+            this.registros.Controls.Add(this.dgvRegistro);
+            this.registros.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.registros.Location = new System.Drawing.Point(34, 203);
+            this.registros.Name = "registros";
+            this.registros.Size = new System.Drawing.Size(990, 290);
+            this.registros.TabIndex = 3;
+            this.registros.TabStop = false;
+            this.registros.Text = "Registros";
+            // 
+            // txtbuscar
+            // 
+            this.txtbuscar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtbuscar.Location = new System.Drawing.Point(205, 40);
+            this.txtbuscar.Name = "txtbuscar";
+            this.txtbuscar.Size = new System.Drawing.Size(100, 30);
+            this.txtbuscar.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(28, 41);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(171, 25);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Buscar (por clave)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1051, 505);
+            this.Controls.Add(this.registros);
+            this.Controls.Add(this.accion);
+            this.Controls.Add(this.Datos);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvRegistro)).EndInit();
+            this.Datos.ResumeLayout(false);
+            this.Datos.PerformLayout();
+            this.accion.ResumeLayout(false);
+            this.registros.ResumeLayout(false);
+            this.registros.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvRegistro;
+        private System.Windows.Forms.GroupBox Datos;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox accion;
+        private System.Windows.Forms.GroupBox registros;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txttelefono;
+        private System.Windows.Forms.TextBox txtapellidos;
+        private System.Windows.Forms.TextBox txtnombres;
+        private System.Windows.Forms.TextBox txtcodigo;
+        private System.Windows.Forms.TextBox txtbuscar;
+        private System.Windows.Forms.Button btnbuscar;
+        private System.Windows.Forms.Button btneliminar;
+        private System.Windows.Forms.Button btnmodificar;
+        private System.Windows.Forms.Button btnagregar;
+    }
+}
+
---
+++ Pantallamaestra/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pantallamaestra
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        BaseDeDatos DB_Registros = new BaseDeDatos();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dgvRegistro.DataSource = DB_Registros.SelectDataTable("select * from personas");
+        }
+
+        private void btnagregar_Click(object sender, EventArgs e)
+        {
+            string agregar = "insert into datos values (" + txtcodigo.Text + ",'" + txtnombres.Text + "','" + txtapellidos.Text + ",'" + txttelefono.Text + "')";
+        
+            if (DB_Registros.executecommand(agregar))
+            {
+                MessageBox.Show("Registro agregado recientemente");
+                dgvRegistro.DataSource = DB_Registros.SelectDataTable("select * from datos");
+            }
+
+            else
+            {
+                MessageBox.Show("Error al agregar");
+            }
+
+        }
+
+        private void btneliminar_Click(object sender, EventArgs e)
+        {
+            string eliminar = "delete datos where codigo = " + txtcodigo.Text;
+
+            if (DB_Registros.executecommand(eliminar))
+            {
+                MessageBox.Show("Registro eliminado correctamente");
+                dgvRegistro.DataSource = DB_Registros.SelectDataTable("select * from datos");
+            }
+
+            else
+            {
+                MessageBox.Show("Error al eliminar");
+            }
+
+        }
+
+        private void btnmodificar_Click(object sender, EventArgs e)
+        {
+            string actualizar = "update datos set nombre =" + txtnombres.Text + "where codigo =" + txtcodigo.Text;
+
+            if (DB_Registros.executecommand(actualizar))
+            {
+                MessageBox.Show("Registro modificado correctamente");
+                dgvRegistro.DataSource = DB_Registros.SelectDataTable("select * from datos");
+            }
+
+            else
+            {
+                MessageBox.Show("Error al modificar");
+            }
+
+        }
+
+        private void btnbuscar_Click(object sender, EventArgs e)
+        {
+            dgvRegistro.DataSource = DB_Registros.SelectDatatable("select * from datos where codigo = " + txtbuscar.Text);
+        }
+
+        private void dgvRegistro_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            DataGridViewRow dgv = dgvRegistro.Rows[e.RowIndex];
+            txtcodigo.Text = dgv.Cells[0].Value.ToString();
+            txtnombres.Text = dgv.Cells[1].Value.ToString();
+            txtapellidos.Text = dgv.Cells[2].Value.ToString();
+            txttelefono.Text = dgv.Cells[3].Value.ToString();
+        }
+
+    }
+}
